87fa0d8d6b63aa6bb07c68dd783ff1b9e1981806
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-01-13 04:23:18 +0900

    Add makingAnagrams function implementation
    Implemented the makingAnagrams function to calculate the number of deletions required to make two strings anagrams.

--- Making-Anagrams.cs
+++ Making-Anagrams.cs
@@ -11,3 +11,56 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(string s1, string s2)
+    {
+        int[] cnt = new int[26];
+
+        foreach (char c in s1) {
+            cnt[c - 'a']++;
+        }
+
+        foreach (char c in s2) {
+            cnt[c - 'a']--;
+        }
+
+        int deletions = 0;
+        foreach (int x in cnt) {
+            deletions += Math.Abs(x);
+        }
+
+        return deletions;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
