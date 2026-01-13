17370980a6ddde734d9c4470aae564b9dd815059
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-01-14 07:31:38 +0900

    Add main method for anagram deletion calculation

--- Making-Anagrams.java
+++ Making-Anagrams.java
@@ -40,3 +40,23 @@ class Result {
         return deletions;
     }
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
